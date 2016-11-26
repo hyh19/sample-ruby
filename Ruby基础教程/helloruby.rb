c269a3b3d1db8d6fc1d8b0a8850a3dae23aaed97
Yuhui Huang <mrhuangyuhui@gmail.com>
2016-11-26 17:25:17 +0800

    学习输出方法

---
+++ Ruby基础教程/helloruby.rb
@@ -0,0 +1,22 @@
+#print("Hello, Ruby.\n")
+
+# 特殊字符转义
+#print("Hello, \"Ruby\".\n")
+
+# 单引号内的特殊字符不转义
+#print('Hello, \"Ruby\".\n')
+
+# 调用方法可省略括号
+#print "Hello, Ruby.\n"
+
+# 连续输出多个字符串，用逗号分隔
+#print "Hello, ", "Ruby.\n"
+
+# puts方法输出字符串会自动换行
+# puts "Hello, Ruby."
+
+# p方法输出不同类型的对象时会区分
+puts "100"
+puts 100
+p "100"
+p 100
